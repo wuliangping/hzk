0108e5c42b8c7d72ffc9c6c6c3ead81ec22e2174
wuliangping <383495521@qq.com>
2019-08-28 10:21:40 +0800

    登录布局完成

--- src/components/login.js
+++ src/components/login.js
@@ -1,5 +1,9 @@
 import React,{Component} from 'react'
 
+// flex布局
+import { Flex,NavBar,Icon,List,InputItem,Button,WingBlank,WhiteSpace} from 'antd-mobile';
+import './login.css'
+
 class Login extends Component{
   constructor(props) {
     super(props)
@@ -7,8 +11,37 @@ class Login extends Component{
   }
 
   render() {
+    // let style = {width:'100%'}
     return <div>
-     登录
+      <Flex direction="column">
+        {/* <Flex.Item style={style}> */}
+        <Flex.Item>
+        <NavBar
+          icon={<Icon type="left" />}
+          onLeftClick={() => console.log('onLeftClick')}
+          rightContent={[
+          <Icon key="0" type="search" style={{ marginRight: '16px' }} />,
+          <Icon key="1" type="ellipsis" />,
+          ]}
+        >登录首页</NavBar>
+        </Flex.Item>
+        <WhiteSpace size="lg"/>
+
+        <Flex.Item>
+          {/* <WingBlank> 两翼留白 */}
+          <WingBlank>
+            <List>
+                <InputItem value="" editable={true}>姓名</InputItem>
+                <InputItem value="" editable={true}>密码</InputItem>
+            </List>
+            {/* <WhiteSpace size="lg"/>上下留白 */}
+            <WhiteSpace size="lg"/>
+
+            <Button type="primary">登录</Button>
+          </WingBlank>
+        </Flex.Item>
+
+      </Flex>
 
     </div>
   }
